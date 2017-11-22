8cd125cef1ce4dc451a6bb9eefcc61ee1ef30f8c
emersion <contact@emersion.fr>
2017-11-22 11:11:27 +0100

    Implement block header and free()

--- mm.c
+++ mm.c
@@ -43,38 +43,93 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-/*
- * mm_init - initialize the malloc package.
- */
+// TODO: header too big (8 bytes)
+
+#define BLOCK_HEADER_SIZE_MASK (~(size_t)1)
+
+inline size_t *block_header(void *block) {
+  return block;
+}
+
+inline size_t block_size(void *block) {
+  size_t *header = block_header(block);
+  return *header & BLOCK_HEADER_SIZE_MASK;
+}
+
+inline size_t block_payload_size(void *block) {
+  size_t size = block_size(block);
+  return size - SIZE_T_SIZE;
+}
+
+inline int block_allocated(void *block) {
+  size_t *header = block_header(block);
+  return *header & 1;
+}
+
+inline void *block_payload(void *block) {
+  return (char *)block + SIZE_T_SIZE;
+}
+
+inline void *block_next(void *block) {
+  return (char *)block + block_size(block);
+}
+
+inline void block_init(void *block, size_t size, int allocated) {
+  size_t *header = block_header(block);
+  *header = size | (size_t)allocated;
+}
+
+void *block_create(size_t payload_size) {
+  int size = ALIGN(payload_size + SIZE_T_SIZE);
+  void *block = mem_sbrk(size);
+  if (block == (void *)-1) {
+    return NULL;
+  }
+
+  block_init(block, size, 1);
+  return block;
+}
+
+void *block_from_payload(void *payload) {
+  return (char *)payload - SIZE_T_SIZE;
+}
+
+inline void block_set_allocated(void *block, int allocated) {
+  size_t *header = block_header(block);
+  *header = block_size(block) | (size_t)allocated;
+}
+
 int mm_init(void) {
   return 0;
 }
 
-/*
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
- */
 void *mm_malloc(size_t size) {
-  int newsize = ALIGN(size + SIZE_T_SIZE);
-  void *p = mem_sbrk(newsize);
-  if (p == (void *)-1) {
-    return NULL;
-  } else {
-    *(size_t *)p = size;
-    return (void *)((char *)p + SIZE_T_SIZE);
+  void *block = mem_heap_lo();
+  void *end = mem_heap_hi();
+  while (block < end) {
+    if (block_payload_size(block) >= size && !block_allocated(block)) {
+      block_set_allocated(block, 1);
+      return block_payload(block);
+    }
+
+    block = block_next(block);
   }
+
+  block = block_create(size);
+  if (block == NULL) {
+    return NULL;
+  }
+
+  return block_payload(block);
 }
 
-/*
- * mm_free - Freeing a block does nothing.
- */
-void mm_free(void *ptr) {
-  // No-op
+void mm_free(void *payload) {
+  if (payload == NULL) {
+    return;
+  }
+  block_set_allocated(block_from_payload(payload), 0);
 }
 
-/*
- * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
- */
 void *mm_realloc(void *ptr, size_t size) {
   void *oldptr = ptr;
   void *newptr;
