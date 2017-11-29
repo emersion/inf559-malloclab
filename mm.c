b6fcb399d050b39db559f123c3b893774a3f60f9
emersion <contact@emersion.fr>
2017-11-29 16:36:04 +0100

    Add docs

--- mm.c
+++ mm.c
@@ -1,14 +1,40 @@
 /*
- * mm-naive.c - The fastest, least memory-efficient malloc package.
+ * A simple memory allocator with an address-ordered explicit free list.
  *
- * In this naive approach, a block is allocated by simply incrementing
- * the brk pointer.  A block is pure payload. There are no headers or
- * footers.  Blocks are never coalesced or reused. Realloc is
- * implemented directly using mm_malloc and mm_free.
+ * Structure of blocks:
  *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
+ *   +---------------+----------------+---------------+
+ *   | size_t header | void **payload | size_t footer |
+ *   +---------------+----------------+---------------+
+ *
+ * The header and footer contain both the block's tag.
+ *
+ * The tag contains the size of the whole block. Since blocks sizes are aligned,
+ * the least significant bit is always 0. It's used as a flag which tells
+ * whether the block is allocated or not.
+ *
+ * Structure of the payload of unallocated blocks:
+ *
+ *   +------------+------------+
+ *   | void *prev | void *next |
+ *   +------------+------------+
+ *
+ * `prev` is a pointer to the previous unallocated block, `next` is a pointer to
+ * the next one.
+ *
+ * The very first block of the heap is allocated on initialization and is called
+ * the root block. It has a payload with the same structure as unallocated
+ * blocks and is used as the head of the free list. In other words, the `prev`
+ * field of the root block points to the last unallocated block and the `next`
+ * field points to th first one.
+ *
+ * The API is split into two groups of functions:
+ * - `block_*` functions abstract blocks. They generally take the block they
+ *   operate on as the first argument. Functions marked as `static` are private
+ *   and shouldn't be used by other functions.
+ * - `mm_*` functions implement the memory allocator.
  */
+
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,45 +44,43 @@
 #include "memlib.h"
 #include "mm.h"
 
-/*********************************************************
- * NOTE TO STUDENTS: Before you do anything else, please
- * provide your team information in the following struct.
- ********************************************************/
 team_t team = {
-  /* Team name */
-  "​👌​👌​",
-  /* First member's full name */
-  "Simon Ser",
-  /* First member's email address */
-  "simon.ser@polytechnique.fr",
-  /* Second member's full name (leave blank if none) */
-  "",
-  /* Second member's email address (leave blank if none) */
-  "",
+  .teamname = "​👌​👌​",
+  .name1 = "Simon Ser",
+  .id1 = "simon.ser@polytechnique.fr",
+  .name2 = "",
+  .id2 = "",
 };
 
-/* single word (4) or double word (8) alignment */
+// Single word (4) or double word (8) alignment
 #define ALIGNMENT 8
 
-/* rounds up to the nearest multiple of ALIGNMENT */
+// Rounds up to the nearest multiple of ALIGNMENT
 #define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
 
+// Size of a whole block
 #define BLOCK_TAG_SIZE (ALIGN(sizeof(size_t)))
+// Tag mask to check if a block is allocated
 #define BLOCK_TAG_ALLOCATED_MASK ((size_t)1)
+// Tag mask to get a block's size
 #define BLOCK_TAG_SIZE_MASK (~(size_t)1)
 
+// A pointer to the root block
 void *block_root = NULL;
 
-inline size_t block_tag(void *block) {
+// Returns the block's tag.
+static inline size_t block_tag(void *block) {
   size_t *header = block;
   return *header;
 }
 
+// Returns the block size in bytes.
 inline size_t block_size(void *block) {
   return block_tag(block) & BLOCK_TAG_SIZE_MASK;
 }
 
-inline void block_set_tag(void *block, size_t size, int allocated) {
+// Sets a block's tag.
+static void block_set_tag(void *block, size_t size, int allocated) {
   size_t tag = size | (size_t)allocated;
 
   size_t *header = (size_t *)block;
@@ -66,18 +90,22 @@ inline void block_set_tag(void *block, size_t size, int allocated) {
   *footer = tag;
 }
 
+// Returns the block's payload size.
 inline size_t block_payload_size(void *block) {
   return block_size(block) - 2*BLOCK_TAG_SIZE;
 }
 
+// Checks whether or not a block is currently allocated.
 inline int block_allocated(void *block) {
   return block_tag(block) & BLOCK_TAG_ALLOCATED_MASK;
 }
 
+// Returns a pointer to the block's payload.
 inline void *block_payload(void *block) {
   return (char *)block + BLOCK_TAG_SIZE;
 }
 
+// Returns the next block in the heap, or NULL if it's the last one.
 void *block_next(void *block) {
   void *next = (char *)block + block_size(block);
   if (next > mem_heap_hi()) {
@@ -86,6 +114,7 @@ void *block_next(void *block) {
   return next;
 }
 
+// Returns the previous block in the heap, or NULL if it's the first one.
 void *block_prev(void *block) {
   if (block <= mem_heap_lo()) {
     return NULL;
@@ -100,6 +129,10 @@ void *block_prev(void *block) {
   return prev;
 }
 
+// Allocates a new block with the given payload size. The newly created block is
+// marked allocated. Blocks cannot be destroyed.
+//
+// Returns NULL on error.
 void *block_create(size_t payload_size) {
   int size = ALIGN(payload_size + 2*BLOCK_TAG_SIZE);
   void *block = mem_sbrk(size);
@@ -111,14 +144,23 @@ void *block_create(size_t payload_size) {
   return block;
 }
 
+// Resizes the block. This function should be used carefully as it doesn't
+// updates pointers to previous and next unallocated blocks. Moreover, it's the
+// caller's responsibility to ensure the new size doesn't make this block end
+// in the middle of another block.
 inline void block_set_size(void *block, size_t size) {
   block_set_tag(block, size, block_allocated(block));
 }
 
+// Returns a block from its payload.
 inline void *block_from_payload(void *payload) {
   return (char *)payload - BLOCK_TAG_SIZE;
 }
 
+// Returns the previous unallocated block, or NULL if there isn't any.
+//
+// This function has a O(1) complexity when used on unallocated or root blocks,
+// but has a O(n) complexity otherwise.
 void *block_prev_unallocated(void *block) {
   if (!block_allocated(block) || block == block_root) {
     void **ptrs = block_payload(block);
@@ -136,6 +178,10 @@ void *block_prev_unallocated(void *block) {
   return NULL;
 }
 
+// Returns the next unallocated block, or NULL if there isn't any.
+//
+// This function has a O(1) complexity when used on unallocated or root blocks,
+// but has a O(n) complexity otherwise.
 void *block_next_unallocated(void *block) {
   if (!block_allocated(block) || block == block_root) {
     void **ptrs = block_payload(block);
@@ -153,6 +199,7 @@ void *block_next_unallocated(void *block) {
   return NULL;
 }
 
+// Sets the previous unallocated block pointer. The block must be unallocated.
 void block_set_prev_unallocated(void *block, void *prev) {
   assert(!block_allocated(block));
   assert(!prev || !block_allocated(prev));
@@ -166,6 +213,7 @@ void block_set_prev_unallocated(void *block, void *prev) {
   }
 }
 
+// Sets the next unallocated block pointer. The block must be unallocated.
 void block_set_next_unallocated(void *block, void *next) {
   assert(!block_allocated(block));
   assert(!next || !block_allocated(next));
@@ -179,9 +227,13 @@ void block_set_next_unallocated(void *block, void *next) {
   }
 }
 
+// Marks the block as allocated.
+//
+// This function takes care of updating the free list pointers as necessary.
 void block_set_allocated(void *block) {
   assert(!block_allocated(block));
 
+  // Update free list pointers
   void *prev_unallocated = block_prev_unallocated(block);
   void *next_unallocated = block_next_unallocated(block);
   if (prev_unallocated != NULL) {
@@ -190,6 +242,7 @@ void block_set_allocated(void *block) {
     block_set_prev_unallocated(next_unallocated, prev_unallocated);
   }
 
+  // Update free list head
   void **root_ptrs = block_payload(block_root);
   if (root_ptrs[1] == block) {
     root_ptrs[1] = next_unallocated;
@@ -198,16 +251,25 @@ void block_set_allocated(void *block) {
     root_ptrs[0] = prev_unallocated;
   }
 
+  // Update block tag
   block_set_tag(block, block_size(block), 1);
 }
 
+// Marks the block as unallocated. `prev` and `next` are pointers to the
+// previous and next unallocated blocks.
+//
+// This function takes care of updating the free list pointers as necessary.
 void block_set_unallocated(void *block, void *prev, void *next) {
   assert(block_allocated(block));
 
+  // Update block tag
   block_set_tag(block, block_size(block), 0);
+
+  // Update free list pointers
   block_set_prev_unallocated(block, prev);
   block_set_next_unallocated(block, next);
 
+  // Update free list head
   void **root_ptrs = block_payload(block_root);
   if (root_ptrs[1] == NULL || block < root_ptrs[1]) {
     root_ptrs[1] = block;
@@ -217,7 +279,7 @@ void block_set_unallocated(void *block, void *prev, void *next) {
   }
 }
 
-
+// Checks that the heap is consistent.
 #if 0 // debug
 void mm_check(void) {
   // Loop through the free list
@@ -264,6 +326,10 @@ inline void mm_check(void) {
 }
 #endif
 
+
+// Initializes the memory allocator. Returns zero on success.
+//
+// This function takes care of allocating and initializing the root block.
 int mm_init(void) {
   block_root = block_create(2 * sizeof(void *));
   if (block_root == NULL) {
@@ -277,6 +343,7 @@ int mm_init(void) {
   return 0;
 }
 
+// Allocates a memory block with a payload of `size` bytes.
 void *mm_malloc(size_t size) {
   mm_check();
 
@@ -284,6 +351,7 @@ void *mm_malloc(size_t size) {
     return NULL;
   }
 
+  // First try to find a large enough unallocated block
   void *block = block_next_unallocated(block_root);
   while (block != NULL) {
     if (block_payload_size(block) >= size) {
@@ -294,6 +362,7 @@ void *mm_malloc(size_t size) {
     block = block_next_unallocated(block);
   }
 
+  // No block available, allocate a new one
   block = block_create(size);
   if (block == NULL) {
     return NULL;
@@ -302,6 +371,9 @@ void *mm_malloc(size_t size) {
   return block_payload(block);
 }
 
+// Frees a memory block.
+//
+// This function coalesces free blocks.
 void mm_free(void *payload) {
   if (payload == NULL) {
     return;
@@ -316,20 +388,24 @@ void mm_free(void *payload) {
   int prev_allocated = prev && block_allocated(prev);
   int next_allocated = next && block_allocated(next);
   if (prev && !prev_allocated && next && !next_allocated) {
+    // Coalesce `prev`, `block` and `next`
     size_t new_size = block_size(prev) + block_size(block) + block_size(next);
     void *next_unallocated = block_next_unallocated(next);
     block_set_size(prev, new_size);
     block_set_next_unallocated(prev, next_unallocated);
   } else if (prev && !prev_allocated) {
+    // Coalesce `prev` and `block`
     size_t new_size = block_size(prev) + block_size(block);
     block_set_size(prev, new_size);
   } else if (next && !next_allocated) {
+    // Coalesce block` and `next`
     size_t new_size = block_size(block) + block_size(next);
     void *prev_unallocated = block_prev_unallocated(next);
     void *next_unallocated = block_next_unallocated(next);
     block_set_size(block, new_size);
     block_set_unallocated(block, prev_unallocated, next_unallocated);
   } else {
+    // Just mark this block as unallocated
     void *prev_unallocated = block_prev_unallocated(block);
     void *next_unallocated = block_next_unallocated(block);
     block_set_unallocated(block, prev_unallocated, next_unallocated);
@@ -338,6 +414,7 @@ void mm_free(void *payload) {
   mm_check();
 }
 
+// Re-allocates a memory block with a new payload size.
 void *mm_realloc(void *payload, size_t size) {
   if (payload == NULL) {
     return mm_malloc(size);
@@ -356,6 +433,7 @@ void *mm_realloc(void *payload, size_t size) {
   size_t new_size = size;
 
   if (old_size >= new_size) {
+    // The current block is already large enough
     // TODO: shrink old block size, add new free block?
     return old_payload;
   }
@@ -367,9 +445,12 @@ void *mm_realloc(void *payload, size_t size) {
     n += block_size(block);
 
     if (old_size + n >= new_size) {
+      // We found enough unallocated blocks right after this one
+      // First mark all blocks as allocated
       for (void *b = block_next(old_block); b != NULL && b <= block; b = block_next(b)) {
         block_set_allocated(b);
       }
+      // Then resize this block
       size_t new_size = block_size(old_block) + n;
       block_set_size(old_block, new_size);
       return old_payload;
@@ -378,6 +459,7 @@ void *mm_realloc(void *payload, size_t size) {
     block = block_next(block);
   }
 
+  // We need to allocate a brand new block, and copy over the payload
   void *new_payload = mm_malloc(new_size);
   if (new_payload == NULL) {
     return NULL;
